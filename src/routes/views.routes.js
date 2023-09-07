eb3ad913b9309e11697cf4e5f556409797ba8d40
betancourtfit <beta.juan.c@gmail.com>
2023-09-06 22:49:09 -0300

    cambio de views.routes.js

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,8 +1,12 @@
 import express from "express";
+import userRouter from './users.routes.js';
+import productRouter from './products.routes.js';
+import cartRouter from './carts.routes.js';
+import messageRouter from './messages.routes.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/static', (req, res) => {
     res.render('chat', {
         js: "chat.js",
         css: "home.css",
@@ -10,5 +14,19 @@ router.get('/', (req, res) => {
         
     });
 })
+
+router.get('/realtimeproducts', (req, res) => {
+    res.render('realTimeProducts', {
+        css: "style.css",
+        title: "Products",
+        js: "realTimeProducts.js"
+
+    })
+})
+
+router.use('/api/users', userRouter)
+router.use('/api/products', productRouter)
+router.use('/api/carts', cartRouter)
+router.use('/api/message', messageRouter)
 
 export default router;
